47de28026f83ef2bdb7f902be3d36a08ab75b653
yisichoco <yisichoco@gmail.com>
2022-05-26 14:58:27 +0800

    Create App.js

---
+++ 程式碼/0526/8.jsx-三元與inline-if/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+function App() {
+  const [total, setTotal] = useState(0)
+
+  return (
+    <>
+      <h1
+        onClick={() => {
+          setTotal(total + 1)
+        }}
+      >
+        {total}
+      </h1>
+      {/* 示範三元表達式 */}
+      <p>{total > 10 ? 'total大於10了' : '目前小於等於10'}</p>
+      {/* 示範inline if語法 */}
+      <p>{total > 5 && 'total大於5了'}</p>
+      {/* 相當於上面的執行結果 */}
+      <p>{total > 5 ? 'total大於5了' : ''}</p>
+    </>
+  )
+}
+
+export default App
