0e5d85330f0d6075f862ddfa72b3e35e92dc6b91
yisichoco <yisichoco@gmail.com>
2022-05-26 14:17:03 +0800

    Update App.js

--- 程式碼/0526/5.jsx-map範例-2-key/App.js
+++ 程式碼/0526/5.jsx-map範例-2-key/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <ul>
+        {/* key值一定要，有id用id，不得已(or靜態資料)才用index值 */}
         {data.map((v, i) => {
           return <li key={v.id}>{v.name}</li>
         })}
